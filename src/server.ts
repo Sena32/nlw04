46ae4489dce99823819ef7cce9da274002a1e393
Ailton Sena <ailtonsenap@gmail.com>
2021-02-25 14:25:29 -0300

    feat: create users.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import 'reflect-metadata';
 import express, { request, response } from 'express';
 import './database';
+import { router } from './routes';
 
 const app = express();
 
 app.listen(3333, ()=>console.log('Server is running'));
+
+// hability express with json
+app.use(express.json())
+
+app.use(router);
